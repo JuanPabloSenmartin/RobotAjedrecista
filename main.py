47909975e6ebadd38d15eda7461f1f8622343e98
Juan Pablo Senmartin <juanpablo.senmartin@ing.austral.edu.ar>
2024-08-18 17:34:40 -0300

    robot movement

--- main.py
+++ main.py
@@ -42,25 +42,62 @@ if __name__ == "__main__":
     try:
         l = 0.05
         
-        joint_angles = rob.getj()
-        print('joint position: ', joint_angles)
-        print('go to start position')
-        movejRobot(robot_startposition)
+        # joint_angles = rob.getj()
+        # print('joint position: ', joint_angles)
+        # print('go to start position')
+        # movejRobot(robot_startposition)
+        # time.sleep(2)
+        # print('go down')
+        # movejRobot(robot_reach_down_position)
+        # time.sleep(2)
+        # print('close gripper')
+        # robotiqgrip.close_gripper()
+        # time.sleep(2)
+        # print('open gripper')
+        # robotiqgrip.open_gripper()
+        # print('go to start position')
+        # movejRobot(robot_startposition) 
+
+        # robot_startposition_tcp = (0,
+        #             0.44,
+        #             0.7,
+        #             math.radians(-60),
+        #             math.radians(90),
+        #             math.radians(0))
+        robot_startposition_tcp2 = [-0.14396865671352163, -0.43562006080319216, 0.5020300254283846, -0.0012213596815924665, 3.116276528481967, 0.03889191563688674]
+        
+        # robot_reach_down_position_tcp = (0,
+        #             0.6,
+        #             0.2,
+        #             math.radians(70),
+        #             math.radians(-300),
+        #             math.radians(0.6))
+
+        pose = rob.getl()
+        print("robot tcp is at: ", pose)
+        print("absolute move in base coordinate ")
+        
+        rob.movel(robot_startposition_tcp2, acc=a, vel=v)
         time.sleep(2)
-        print('go down')
-        movejRobot(robot_reach_down_position)
+        pose = robot_startposition_tcp2
+        pose[2] = pose[2] - 0.3
+        rob.movel(pose, acc=a, vel=v)
         time.sleep(2)
         print('close gripper')
         robotiqgrip.close_gripper()
         time.sleep(2)
         print('open gripper')
         robotiqgrip.open_gripper()
-        print('go to start position')
-        movejRobot(robot_startposition)  
-        # print("robot tcp is at: ", pose)
-        # print("absolute move in base coordinate ")
-        # pose[2] += l
-        # rob.movel(pose, acc=a, vel=v)
+        pose[2] = pose[2] + 0.3
+        rob.movel(pose, acc=a, vel=v)
+        time.sleep(2)
+        
+        # time.sleep(2)
+        # rob.movel(robot_reach_down_position_tcp, acc=a, vel=v)
+        # time.sleep(2)
+        # rob.movel(robot_startposition_tcp, acc=a, vel=v)
+        
+
         # print("relative move in base coordinate ")
         # rob.translate((0, 0, -l), acc=a, vel=v)
         # print("relative move back and forth in tool coordinate")
