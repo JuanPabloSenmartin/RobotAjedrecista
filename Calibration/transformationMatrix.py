382515284fc88d89b3ba895fb83719741f260fdc
Agustin <agustin.titotto@ing.austral.edu.ar>
2025-03-10 10:49:58 -0300

    Clean transformationMatrix

--- Calibration/transformationMatrix.py
+++ Calibration/transformationMatrix.py
@@ -5,71 +5,69 @@ import os
 from datetime import datetime
 
 # Read calibration parameters
-def get_calibration_parameters() :
+def get_calibration_parameters():
     input_file = os.path.join('parameters', 'intrinsic_parameters.yaml')
     with open(input_file, 'r') as file:
         data = yaml.safe_load(file)  
-    K = np.array(data.get('K'))
-    distCoef = np.array(data.get('distCoef'))
-    return K, distCoef
+    intrinsic_matrix = np.array(data.get('intrinsic_matrix'))
+    distortion_coefficients = np.array(data.get('distortion_coefficients'))
+    return intrinsic_matrix, distortion_coefficients
 
-def guardar_imagen(frame, carpeta='fotos'):
-    if not os.path.exists(carpeta):
-        os.makedirs(carpeta)
-    nombre_archivo = datetime.now().strftime("foto_%Y%m%d_%H%M%S.png")
-    ruta_completa = os.path.join(carpeta, nombre_archivo)
-    cv.imwrite(ruta_completa, frame)
-    print(f"Imagen guardada en: {ruta_completa}")
-    return ruta_completa
+# Save image
+def save_image(frame, folder='photos'):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    file_name = datetime.now().strftime("photo_%Y%m%d_%H%M%S.png")
+    full_path = os.path.join(folder, file_name)
+    cv.imwrite(full_path, frame)
+    print(f"Image saved at: {full_path}")
+    return full_path
 
-img_pts = np.zeros((4, 2), dtype="float32")
+image_points = np.zeros((4, 2), dtype="float32")
 
+# Get image coordinates
 def get_image_coordinates(saved_corners):
-
-    # Cálculo de las esquinas
     upper_left_corner = saved_corners[0][0]
     upper_right_corner = saved_corners[6][0]
     lower_left_corner = saved_corners[42][0]
     lower_right_corner = saved_corners[48][0]
     
-    img_pts = np.array([
-        [upper_left_corner[0], upper_left_corner[1]],  # esquina superior izquierda
-        [upper_right_corner[0], upper_right_corner[1]],  # esquina superior derecha
-        [lower_left_corner[0], lower_left_corner[1]],  # esquina inferior izquierda
-        [lower_right_corner[0], lower_right_corner[1]]   # esquina inferior derecha
+    image_points = np.array([
+        [upper_left_corner[0], upper_left_corner[1]],
+        [upper_right_corner[0], upper_right_corner[1]],
+        [lower_left_corner[0], lower_left_corner[1]],
+        [lower_right_corner[0], lower_right_corner[1]]
     ], dtype="float32")
 
-    print(str(img_pts))
-    return img_pts
+    print(str(image_points))
+    return image_points
 
-# Coordinates in robot system
-robot_pts = np.array([
-    [201, -418],  # esquina superior izquierda
-    [210, -119],  # esquina superior derecha
-    [498, -427],   # esquina inferior izquierda
-    [509, -127]  # esquina inferior derecha
+# Complete coordinates in robot system
+robot_points = np.array([
+    [201, -418],
+    [210, -119],
+    [498, -427],
+    [509, -127]
 ], dtype="float32")
 
-# ------------------------- Primary loop ------------------------- #
+camera = cv.VideoCapture(2)
 
-cam = cv.VideoCapture(2)
+chessboard_size = (7, 7)
 
-chessBoard = (7,7)
-
-width = cam.get(cv.CAP_PROP_FRAME_WIDTH)
-height = cam.get(cv.CAP_PROP_FRAME_HEIGHT)
-newSize = (640, int(640 * height / width))
+frame_width = camera.get(cv.CAP_PROP_FRAME_WIDTH)
+frame_height = camera.get(cv.CAP_PROP_FRAME_HEIGHT)
+new_size = (640, int(640 * frame_height / frame_width))
 
 while True:
-    ret, im = cam.read()
-    if ret:                                                     # For testing
-        imLowRes = cv.resize(im, newSize)
-        imGrayLowRes = cv.cvtColor(imLowRes, cv.COLOR_BGR2GRAY)
-        ret, corners = cv.findChessboardCorners(imGrayLowRes, chessBoard, None)
+    ret, frame = camera.read()
+    if ret:
+        frame_low_res = cv.resize(frame, new_size)
+        frame_gray_low_res = cv.cvtColor(frame_low_res, cv.COLOR_BGR2GRAY)
+        ret, corners = cv.findChessboardCorners(frame_gray_low_res, chessboard_size, None)
         if ret:
-            cv.drawChessboardCorners(imLowRes, chessBoard, corners, ret)
+            cv.drawChessboardCorners(frame_low_res, chessboard_size, corners, ret)
     
-    cv.imshow('Cam', imLowRes)
+    cv.imshow('camera', frame_low_res)
 
     key = cv.waitKey(30)
 
@@ -77,41 +75,21 @@ while True:
         key = chr(key)
         match key:
             case ' ':
-                K, distCoef = get_calibration_parameters()
-                imGray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-                guardar_imagen(imGray)
-               # img_pts1 = get_image_coordinates(imGray)
-                undistorted_img = cv.undistort(im, K, distCoef)
-                guardar_imagen(undistorted_img)                 # For testing
-                ret_2, undisorted_corners = cv.findChessboardCorners(undistorted_img, chessBoard, None)
-                img_pts = get_image_coordinates(undisorted_corners)
+                intrinsic_matrix, distortion_coefficients = get_calibration_parameters()
+                frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                undistorted_image = cv.undistort(frame, intrinsic_matrix, distortion_coefficients)
+                ret_2, undistorted_corners = cv.findChessboardCorners(undistorted_image, chessboard_size, None)
+                image_points = get_image_coordinates(undistorted_corners)
             case 'm':
-                matrix = cv.getPerspectiveTransform(img_pts, robot_pts)
-                # Write K and distCoef to a YAML file
+                transformation_matrix = cv.getPerspectiveTransform(image_points, robot_points)
                 output_file = os.path.join('parameters', 'transformation_matrix.yaml')
                 with open(output_file, 'w') as file:
                     yaml.dump({
-                        'matrix': matrix.tolist()
+                        'matrix': transformation_matrix.tolist()
                     }, file)
                 print(f"Matrix saved to {output_file}")
             case 'e':
                 break
 
-cam.release()
+camera.release()
 cv.destroyAllWindows()
-
-
-"""
-matrix = cv.getPerspectiveTransform(img_pts, robot_pts) # Sacamos la matriz de "transformacion"
-print(matrix)
-
-point1 = np.array([[[669.6579, 337.3834]]], dtype="float32")
-point2 = np.array([[[843.5678, 147.28915]]], dtype="float32")
-point3 = np.array([[[819.543, 236.49738]]], dtype="float32")
-
-transformed_point1 = cv.perspectiveTransform(point1, matrix) # Transformamos los puntos de la imagen al sistema del robot
-transformed_point2 = cv.perspectiveTransform(point2, matrix)
-transformed_point3 = cv.perspectiveTransform(point3, matrix)
-"""
-
-
